44438d86fa8c46d1c52d0b51b45d66fcdba352b9
vashistaarav1611 <77904894+vashistaarav1611@users.noreply.github.com>
2021-10-25 17:28:53 -0400

    Update index.py

--- File Editor/index.py
+++ File Editor/index.py
@@ -25,7 +25,7 @@ def save_file():
     with open(filepath, "w") as output_file:
         text = txt_edit.get(1.0, tk.END)
         output_file.write(text)
-    window.title(f"Simple Text Editor - {filepath}")
+    window.title(f"Text||All Editor - {filepath}")
 
 window = tk.Tk()
 window.title("Simple Text Editor")
